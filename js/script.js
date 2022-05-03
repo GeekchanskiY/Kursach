299d85fa7cf177623888e2da7497a7c6a9abab03
GeekchanskiY <astrovskiy.04@gmail.com>
2022-05-03 19:12:38 +0300

    added cryptos

--- js/script.js
+++ js/script.js
@@ -2,9 +2,11 @@ $.get('https://raw.githubusercontent.com/GeekchanskiY/Kursach/master/xml/intro-p
   .done(function(data){
     // parse the xml
     data = $.parseXML(data);
-    $(data).find("currency").each(
+    alert($(data).find("crypto").length)
+    $(data).find("crypto").each(
         function(index, element){
-            let field = $(element)
+            let field = $(element).find("name")
+            alert(field.text())
             let newblock = document.createElement("div")
             newblock.classList.add("plate");
             newblock.innerHTML = "<span>YAY</span>"
