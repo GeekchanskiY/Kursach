63af1f24e347883bfbf34c86407afacbfff80dca
GeekchanskiY <astrovskiy.04@gmail.com>
2022-05-03 17:00:45 +0300

    xml test db created, template base created. Sass init

--- js/script.js
+++ js/script.js
@@ -1,9 +1,16 @@
-$.get('xml/intro-plates.xml')
+$.get('https://raw.githubusercontent.com/GeekchanskiY/Kursach/master/xml/intro-plates.xml')
   .done(function(data){
     // parse the xml
     data = $.parseXML(data);
-    //
-    // do anything you want with the parsed data
+    $(data).find("currency").each(
+        function(index, element){
+            let field = $(element)
+            let newblock = document.createElement("div")
+            newblock.classList.add("plate");
+            newblock.innerHTML = "<span>YAY</span>"
+            document.getElementsByClassName("plates")[0].appendChild(newblock);
+        }
+    )
   })
   .fail(function(){
     alert('something went wrong!');
